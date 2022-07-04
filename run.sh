496109cd307bffc32f062bd033d9cc9bf1320d35
thomaswoehlke <thomas.woehlke@gmail.com>
2022-07-04 18:12:32 +0200

    work

--- run.sh
+++ run.sh
@@ -15,7 +15,11 @@ function siteAll() {
 }
 
 function secondSetup() {
-		$MY_MVN dependency:purge-local-repository clean dependency:resolve dependency:resolve-plugins dependency:tree dependency:sources dependency:analyze-dep-mgt dependency:analyze-report install site site:deploy liberty:dev
+
+    $MY_MVN dependency:purge-local-repository clean dependency:resolve dependency:resolve-plugins dependency:tree dependency:sources dependency:analyze-dep-mgt dependency:analyze-report install
+    $MY_MVN -Pliberty dependency:purge-local-repository clean dependency:resolve dependency:resolve-plugins dependency:tree dependency:sources dependency:analyze-dep-mgt dependency:analyze-report install site site:deploy liberty:package
+    $MY_MVN -Pwildfly dependency:purge-local-repository clean dependency:resolve dependency:resolve-plugins dependency:tree dependency:sources dependency:analyze-dep-mgt dependency:analyze-report install site site:deploy
+    $MY_MVN -Ppayara dependency:purge-local-repository clean dependency:resolve dependency:resolve-plugins dependency:tree dependency:sources dependency:analyze-dep-mgt dependency:analyze-report install site site:deploy
 }
 
 function firstSetup() {
