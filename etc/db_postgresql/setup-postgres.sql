40b3e09b2d0929beb6dde068ba0c4cc8f9ca5ff6
thomaswoehlke <thomas.woehlke@gmail.com>
2022-07-09 05:57:16 +0200

    work

--- etc/db_postgresql/setup-postgres.sql
+++ etc/db_postgresql/setup-postgres.sql
@@ -2,36 +2,36 @@
 
 -- DROP ROLE java;
 
-CREATE ROLE jakartaee_petclinic LOGIN
-    PASSWORD 'jakartaee_petclinicpwd'
+CREATE ROLE petclinic_jakartaee LOGIN
+    PASSWORD 'petclinic_jakartaeepwd'
     SUPERUSER INHERIT CREATEDB CREATEROLE REPLICATION;
-GRANT pg_monitor TO jakartaee_petclinic;
-GRANT pg_read_all_settings TO jakartaee_petclinic;
-GRANT pg_read_all_stats TO jakartaee_petclinic;
-GRANT pg_signal_backend TO jakartaee_petclinic;
-GRANT pg_stat_scan_tables TO jakartaee_petclinic;
+GRANT pg_monitor TO petclinic_jakartaee;
+GRANT pg_read_all_settings TO petclinic_jakartaee;
+GRANT pg_read_all_stats TO petclinic_jakartaee;
+GRANT pg_signal_backend TO petclinic_jakartaee;
+GRANT pg_stat_scan_tables TO petclinic_jakartaee;
 
-CREATE TABLESPACE tablespace_jakartaee_petclinic
+CREATE TABLESPACE tablespace_petclinic_jakartaee
     OWNER tw
-    LOCATION '/opt/postgresql/tablespace_jakartaee_petclinic';
+    LOCATION '/opt/postgresql/tablespace_petclinic_jakartaee';
 
-ALTER TABLESPACE tablespace_jakartaee_petclinic
-    OWNER TO jakartaee_petclinic;
+ALTER TABLESPACE tablespace_petclinic_jakartaee
+    OWNER TO petclinic_jakartaee;
 
 
--- Database: jakartaee_petclinic
+-- Database: petclinic_jakartaee
 
--- DROP DATABASE jakartaee_petclinic;
+-- DROP DATABASE petclinic_jakartaee;
 -- CONNECTION LIMIT = -1;
 
-CREATE DATABASE jakartaee_petclinic
-    WITH OWNER = jakartaee_petclinic
+CREATE DATABASE petclinic_jakartaee
+    WITH OWNER = petclinic_jakartaee
     ENCODING = 'UTF8'
-    TABLESPACE = tablespace_jakartaee_petclinic
+    TABLESPACE = tablespace_petclinic_jakartaee
     CONNECTION LIMIT = -1;
 
 CREATE DATABASE petclinic_jakartaee_wildfly
-    WITH OWNER = jakartaee_petclinic
+    WITH OWNER = petclinic_jakartaee
     ENCODING = 'UTF8'
-    TABLESPACE = tablespace_jakartaee_petclinic
+    TABLESPACE = tablespace_petclinic_jakartaee
     CONNECTION LIMIT = -1;
