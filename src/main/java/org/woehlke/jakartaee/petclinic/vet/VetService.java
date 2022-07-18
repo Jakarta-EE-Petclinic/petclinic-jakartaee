642208a43412ef27e491fb8451af4c4fc744ba76
thomaswoehlke <thomas.woehlke@gmail.com>
2022-07-18 18:51:24 +0200

    work

---
+++ src/main/java/org/woehlke/jakartaee/petclinic/vet/VetService.java
@@ -0,0 +1,10 @@
+package org.woehlke.jakartaee.petclinic.vet;
+
+import org.woehlke.jakartaee.petclinic.application.framework.db.CrudService;
+import org.woehlke.jakartaee.petclinic.application.framework.db.SearchableService;
+
+import java.io.Serializable;
+
+public interface VetService extends CrudService<Vet>, SearchableService<Vet>, Serializable {
+    long serialVersionUID = 6211608636423556157L;
+}
