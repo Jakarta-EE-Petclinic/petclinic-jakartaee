51e1ebbece722f993173fd44a2785becf32f6641
thomaswoehlke <thomas.woehlke@gmail.com>
2022-07-18 22:11:20 +0200

    work

--- src/main/java/org/woehlke/jakartaee/petclinic/application/conf/PetclinicApplication.java
+++ src/main/java/org/woehlke/jakartaee/petclinic/application/conf/PetclinicApplication.java
@@ -7,6 +7,7 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Named;
 import jakarta.security.enterprise.authentication.mechanism.http.BasicAuthenticationMechanismDefinition;
 import jakarta.security.enterprise.identitystore.DatabaseIdentityStoreDefinition;
+import jakarta.security.enterprise.identitystore.IdentityStore;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 import lombok.extern.java.Log;
@@ -26,8 +27,9 @@ import java.util.ResourceBundle;
 @ApplicationScoped
 @BasicAuthenticationMechanismDefinition()
 @DatabaseIdentityStoreDefinition(
+        dataSourceLookup = "jdbc/PostgreSQLDS",
+        useFor=IdentityStore.ValidationType.VALIDATE,
         callerQuery = "select password from users where username = ?",
-        groupsQuery = "select GROUPNAME from groups where username = ?",
         priority=30)
 @ApplicationPath("/rest")
 public class PetclinicApplication extends Application implements Serializable {
