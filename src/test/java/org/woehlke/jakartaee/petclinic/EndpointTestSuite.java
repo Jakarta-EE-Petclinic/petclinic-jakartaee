3c029b882d91e603485516c67f7af9caa5c30385
thomaswoehlke <thomas.woehlke@gmail.com>
2022-07-19 22:48:13 +0200

    Junit

--- src/test/java/org/woehlke/jakartaee/petclinic/EndpointTestSuite.java
+++ src/test/java/org/woehlke/jakartaee/petclinic/EndpointTestSuite.java
@@ -1,4 +1,18 @@
 package org.woehlke.jakartaee.petclinic;
 
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+import org.woehlke.jakartaee.petclinic.owner.api.OwnerEndpointIT;
+import org.woehlke.jakartaee.petclinic.pet.api.PetEndpointIT;
+import org.woehlke.jakartaee.petclinic.pettype.api.PetTypeEndpointIT;
+import org.woehlke.jakartaee.petclinic.specialty.api.SpecialtyEndpointIT;
+import org.woehlke.jakartaee.petclinic.vet.api.VetEndpointIT;
+import org.woehlke.jakartaee.petclinic.visit.api.VisitEndpointIT;
+
+@Suite
+@SelectClasses({
+        SpecialtyEndpointIT.class, VetEndpointIT.class,
+        PetTypeEndpointIT.class, PetEndpointIT.class, VisitEndpointIT.class, OwnerEndpointIT.class
+})
 public class EndpointTestSuite {
 }
--- src/test/java/org/woehlke/jakartaee/petclinic/EntityUnitTestSuite.java
+++ src/test/java/org/woehlke/jakartaee/petclinic/EntityUnitTestSuite.java
@@ -1,7 +1,17 @@
 package org.woehlke.jakartaee.petclinic;
 
+import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
+import org.woehlke.jakartaee.petclinic.owner.OwnerUnitTest;
+import org.woehlke.jakartaee.petclinic.pet.PetUnitTest;
+import org.woehlke.jakartaee.petclinic.pettype.PetTypeUnitTest;
+import org.woehlke.jakartaee.petclinic.specialty.SpecialtyUnitTest;
+import org.woehlke.jakartaee.petclinic.vet.VetUnitTest;
 
 @Suite
+@SelectClasses({
+        SpecialtyUnitTest.class,  VetUnitTest.class,
+        PetTypeUnitTest.class, PetUnitTest.class, OwnerUnitTest.class
+})
 public class EntityUnitTestSuite {
 }
