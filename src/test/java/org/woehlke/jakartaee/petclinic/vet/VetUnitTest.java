4df9e72b537241c4bbc0a466321bd458a2c012a8
thomaswoehlke <thomas.woehlke@gmail.com>
2022-07-20 09:49:38 +0200

    Junit

--- src/test/java/org/woehlke/jakartaee/petclinic/vet/VetUnitTest.java
+++ src/test/java/org/woehlke/jakartaee/petclinic/vet/VetUnitTest.java
@@ -1,12 +1,14 @@
 package org.woehlke.jakartaee.petclinic.vet;
 
-import junit.framework.Assert;
+
 import lombok.extern.java.Log;
 import org.junit.jupiter.api.*;
 import org.woehlke.jakartaee.petclinic.application.conf.AbstractEntityTest;
 import org.woehlke.jakartaee.petclinic.specialty.Specialty;
 
 import java.util.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @Log
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -16,8 +18,8 @@ public class VetUnitTest extends AbstractEntityTest {
     @Order(1)
     void runEntityTest01(){
         log.info("runEntityTest01");
-        Assert.assertFalse(uuid01.compareTo(uuid02)==0);
-        Assert.assertFalse(name01.compareTo(name02)==0);
+        assertThat(uuid01.compareTo(uuid02)!=0);
+        assertThat(name01.compareTo(name02)!=0);
         Vet o01 = new Vet();
         o01.setLastName(name01);
         o01.setFirstName(name02);
@@ -26,16 +28,16 @@ public class VetUnitTest extends AbstractEntityTest {
         o02.setLastName(name01);
         o02.setFirstName(name02);
         o02.setUuid(uuid02);
-        Assert.assertTrue(o01.compareTo(o02)==0);
+        assertThat(o01.compareTo(o02)==0);
     }
 
     @Test
     @Order(2)
     void runEntityTest02(){
         log.info("runEntityTest02");
-        Assert.assertFalse(name01.compareTo(name02)==0);
-        Assert.assertFalse(uuid01.compareTo(uuid02)==0);
-        Assert.assertFalse(id01.compareTo(id02)==0);
+        assertThat(name01.compareTo(name02)!=0);
+        assertThat(uuid01.compareTo(uuid02)!=0);
+        assertThat(id01.compareTo(id02)!=0);
         Vet o01 = new Vet();
         o01.setLastName(name01);
         o01.setFirstName(name02);
@@ -46,15 +48,15 @@ public class VetUnitTest extends AbstractEntityTest {
         o02.setFirstName(name02);
         o02.setUuid(uuid02);
         o02.setId(id02);
-        Assert.assertTrue(o01.compareTo(o02)==0);
+        assertThat(o01.compareTo(o02)==0);
     }
 
     @Test
     @Order(3)
     void runEntityTest11(){
         log.info("runEntityTest11");
-        Assert.assertFalse(name01.compareTo(name02)==0);
-        Assert.assertFalse(uuid01.compareTo(uuid02)==0);
+        assertThat(name01.compareTo(name02)!=0);
+        assertThat(uuid01.compareTo(uuid02)!=0);
         Vet o01 = new Vet();
         o01.setLastName(name01);
         o01.setFirstName(name01);
@@ -63,16 +65,16 @@ public class VetUnitTest extends AbstractEntityTest {
         o02.setLastName(name02);
         o02.setFirstName(name02);
         o02.setUuid(uuid02);
-        Assert.assertFalse(o01.compareTo(o02)==0);
+        assertThat(o01.compareTo(o02)!=0);
     }
 
     @Test
     @Order(4)
     void runEntityTest12(){
         log.info("runEntityTest12");
-        Assert.assertFalse(name01.compareTo(name02)==0);
-        Assert.assertFalse(uuid01.compareTo(uuid02)==0);
-        Assert.assertFalse(id01.compareTo(id02)==0);
+        assertThat(name01.compareTo(name02)!=0);
+        assertThat(uuid01.compareTo(uuid02)!=0);
+        assertThat(id01.compareTo(id02)!=0);
         Vet o01 = new Vet();
         o01.setLastName(name01);
         o01.setFirstName(name01);
@@ -83,14 +85,14 @@ public class VetUnitTest extends AbstractEntityTest {
         o02.setFirstName(name02);
         o02.setUuid(uuid02);
         o02.setId(id02);
-        Assert.assertFalse(o01.compareTo(o02)==0);
+        assertThat(o01.compareTo(o02)!=0);
     }
 
     @Test
     @Order(5)
     void runEntityTest21(){
         log.info("runEntityTest21");
-        Assert.assertFalse(name01.compareTo(name02)==0);
+        assertThat(name01.compareTo(name02)!=0);
         Vet o01 = new Vet();
         o01.setLastName(name01);
         o01.setFirstName(name01);
@@ -99,14 +101,14 @@ public class VetUnitTest extends AbstractEntityTest {
         o02.setLastName(name02);
         o02.setFirstName(name02);
         o02.setUuid(uuid);
-        Assert.assertFalse(o01.compareTo(o02)==0);
+        assertThat(o01.compareTo(o02)!=0);
     }
 
     @Test
     @Order(6)
     void runEntityTest22(){
         log.info("runEntityTest22");
-        Assert.assertFalse(name01.compareTo(name02)==0);
+        assertThat(name01.compareTo(name02)!=0);
         Vet o01 = new Vet();
         o01.setLastName(name01);
         o01.setFirstName(name01);
@@ -117,7 +119,7 @@ public class VetUnitTest extends AbstractEntityTest {
         o02.setFirstName(name02);
         o02.setUuid(uuid);
         o02.setId(id);
-        Assert.assertFalse(o01.compareTo(o02)==0);
+        assertThat(o01.compareTo(o02)!=0);
     }
 
     @Test
@@ -125,13 +127,13 @@ public class VetUnitTest extends AbstractEntityTest {
     void runEntityTest99(){
         log.info("runEntityTest99");
         Collections.sort(vetList);
-        Assert.assertTrue(vetList.size() > 2);
+        assertThat(vetList.size() > 2);
         Iterator<Vet> i =  vetList.iterator();
         Vet firstEntity = i.next();
         while(i.hasNext()){
             Vet secondEntity = i.next();
             int compared = secondEntity.compareTo(firstEntity);
-            Assert.assertTrue(compared > 0);
+            assertThat(compared > 0);
             firstEntity = secondEntity;
         }
     }
@@ -143,7 +145,7 @@ public class VetUnitTest extends AbstractEntityTest {
         int expectedSize  = 0;
         Vet entity = new Vet();
         int size = entity.getSpecialties().size();
-        Assert.assertEquals(expectedSize,size);
+        assertThat(expectedSize==size);
     }
 
     @Test
@@ -153,13 +155,12 @@ public class VetUnitTest extends AbstractEntityTest {
         int expectedSize  = 1;
         Vet entity = new Vet();
         String specialtyName = AbstractEntityTest.specialtyNames[0];
-        Assert.assertEquals("Surgeon",specialtyName);
+        assertThat("Surgeon".compareTo(specialtyName)==0);
         Specialty specialty = new Specialty();
         specialty.setName(specialtyName);
         entity.addSpecialty(specialty);
         int size = entity.getSpecialties().size();
-        Assert.assertEquals(expectedSize,size);
-
+        assertThat(expectedSize == size);
     }
 
     @Test
@@ -168,12 +169,12 @@ public class VetUnitTest extends AbstractEntityTest {
         log.info("testGetSpecialtiesAsList03");
         Vet entity = new Vet();
         String specialtyName =  AbstractEntityTest.specialtyNames[0];
-        Assert.assertEquals("Surgeon",specialtyName);
+        assertThat("Surgeon".compareTo(specialtyName)==0);
         Specialty specialty = new Specialty();
         specialty.setName(specialtyName);
         entity.addSpecialty(specialty);
         specialty = entity.getSpecialties().iterator().next();
-        Assert.assertEquals(specialtyName, specialty.getName());
+        assertThat(specialtyName == specialty.getName());
     }
 
     @Test
@@ -184,7 +185,7 @@ public class VetUnitTest extends AbstractEntityTest {
         int expectedSize  = specialtySet.size();
         entity.setSpecialties(specialtySet);
         int size = entity.getSpecialtiesAsList().size();
-        Assert.assertEquals(expectedSize,size);
+        assertThat(expectedSize == size);
     }
 
     @Test
@@ -194,14 +195,14 @@ public class VetUnitTest extends AbstractEntityTest {
         Vet entity = new Vet();
         entity.setSpecialties(specialtySet);
         List<Specialty> listSpecialty = entity.getSpecialtiesAsList();
-        Assert.assertEquals(specialtySet.size(), listSpecialty.size());
-        Assert.assertTrue(listSpecialty.size() > 2);
+        assertThat(specialtySet.size() == listSpecialty.size());
+        assertThat(listSpecialty.size() > 2);
         Iterator<Specialty> i =  listSpecialty.iterator();
         Specialty firstSpecialty = i.next();
         while(i.hasNext()){
             Specialty secondSpecialty = i.next();
             int compared = secondSpecialty.compareTo(firstSpecialty);
-            Assert.assertTrue(compared > 0);
+            assertThat(compared > 0);
             firstSpecialty = secondSpecialty;
         }
     }
