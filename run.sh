a222295b698faf9f0d53362fbfa6c1ae8c4cfe43
thomaswoehlke <thomas.woehlke@gmail.com>
2022-08-08 17:18:55 +0200

    work

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 MY_MVN="./mvnw"
+DEPS="dependency:purge-local-repository clean dependency:resolve dependency:resolve-plugins dependency:tree dependency:sources dependency:analyze-dep-mgt dependency:analyze-report install"
+SITE="site:dite site:deploy site:jar"
 
 function dockerUp() {
     $MY_MVN docker-compose:up
@@ -11,13 +13,13 @@ function dockerDown() {
 }
 
 function doSetup() {
-    $MY_MVN dependency:purge-local-repository clean dependency:resolve dependency:resolve-plugins dependency:tree dependency:sources dependency:analyze-dep-mgt dependency:analyze-report install
-    $MY_MVN -Pliberty dependency:purge-local-repository clean dependency:resolve dependency:resolve-plugins dependency:tree dependency:sources dependency:analyze-dep-mgt dependency:analyze-report install site site:deploy liberty:help
-    $MY_MVN -Pwildfly dependency:purge-local-repository clean dependency:resolve dependency:resolve-plugins dependency:tree dependency:sources dependency:analyze-dep-mgt dependency:analyze-report install site site:deploy wildfly:help
-    $MY_MVN -glassfish dependency:purge-local-repository clean dependency:resolve dependency:resolve-plugins dependency:tree dependency:sources dependency:analyze-dep-mgt dependency:analyze-report install site site:deploy
-    $MY_MVN -Ppayara dependency:purge-local-repository clean dependency:resolve dependency:resolve-plugins dependency:tree dependency:sources dependency:analyze-dep-mgt dependency:analyze-report install site site:deploy
-    $MY_MVN -Ptomee dependency:purge-local-repository clean dependency:resolve dependency:resolve-plugins dependency:tree dependency:sources dependency:analyze-dep-mgt dependency:analyze-report install site site:deploy
-    $MY_MVN -Parc-wildfly-managed dependency:purge-local-repository clean dependency:resolve dependency:resolve-plugins dependency:tree dependency:sources dependency:analyze-dep-mgt dependency:analyze-report install site site:deploy
+    $MY_MVN $DEPS $SITE
+    $MY_MVN -Pliberty $DEPS $SITE liberty:package liberty:help
+    $MY_MVN -Pwildfly $DEPS $SITE cargo:uberwar
+    $MY_MVN -glassfish $DEPS $SITE cargo:uberwar
+    $MY_MVN -Ppayara $DEPS $SITE cargo:uberwar
+    $MY_MVN -Ptomee $DEPS $SITE cargo:uberwar
+    $MY_MVN -Parc-wildfly-managed $DEPS $SITE
 }
 
 function doRun() {
