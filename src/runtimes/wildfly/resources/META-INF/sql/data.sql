cecbec7e3f3cfadbbd43767152282499be644e93
thomaswoehlke <thomas.woehlke@gmail.com>
2022-08-09 23:01:35 +0200

    work

--- src/runtimes/wildfly/resources/META-INF/sql/data.sql
+++ src/runtimes/wildfly/resources/META-INF/sql/data.sql
@@ -5,17 +5,6 @@
 -- Dumped from database version 14.4 (Ubuntu 14.4-1.pgdg22.04+1)
 -- Dumped by pg_dump version 14.4 (Ubuntu 14.4-1.pgdg22.04+1)
 
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
 --
 -- Data for Name: owner; Type: TABLE DATA; Schema: public; Owner: petclinic_jakartaee
 --
