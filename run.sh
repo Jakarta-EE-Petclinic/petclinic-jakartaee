2c754fcc14a18774f458879901ebb428a597d0e5
thomaswoehlke <thomas.woehlke@gmail.com>
2022-08-09 16:11:07 +0200

    work

--- run.sh
+++ run.sh
@@ -4,14 +4,6 @@ MY_MVN="./mvnw"
 DEPS="dependency:purge-local-repository clean dependency:resolve dependency:resolve-plugins dependency:tree dependency:sources dependency:analyze-dep-mgt dependency:analyze-report install"
 SITE="site:site site:jar"
 
-function dockerUp() {
-    $MY_MVN docker-compose:up
-}
-
-function dockerDown() {
-    $MY_MVN docker-compose:down
-}
-
 function doSetup() {
     $MY_MVN $DEPS $SITE
     $MY_MVN -Pliberty $DEPS $SITE liberty:package
