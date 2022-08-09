67a7940fc8f8a28242fcfbac7c5f30d2d316d867
thomaswoehlke <thomas.woehlke@gmail.com>
2022-08-09 16:10:03 +0200

    work

--- run-it.sh
+++ run-it.sh
@@ -6,8 +6,4 @@ export POSTGRESQL_DATABASE=petclinic_jakartaee_wildfly
 export POSTGRESQL_USER=petclinic_jakartaee                                                                                                                                                                                                                                                                                                                                                                                                                                                                aee
 export POSTGRESQL_PASSWORD=petclinic_jakartaeepwd
 
-#./mvnw -Pwildfly-docker docker-compose:up
-
 ./mvnw -Parq-wildfly-managed
-
-#./mvnw -Pwildfly-docker docker-compose:down
