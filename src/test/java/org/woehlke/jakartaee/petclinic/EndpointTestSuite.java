92a02c0d6d620fd305b50bec99712c9bf127d9a2
thomaswoehlke <thomas.woehlke@gmail.com>
2022-08-09 20:04:45 +0200

    work

--- src/test/java/org/woehlke/jakartaee/petclinic/EndpointTestSuite.java
+++ src/test/java/org/woehlke/jakartaee/petclinic/EndpointTestSuite.java
@@ -1,9 +1,13 @@
 package org.woehlke.jakartaee.petclinic;
 
+import lombok.extern.java.Log;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 import org.woehlke.jakartaee.petclinic.it.api.*;
 
+@Log
 @Suite
 @SelectClasses({
     SpecialtyEndpointIT.class, VetEndpointIT.class,
@@ -11,4 +15,14 @@ import org.woehlke.jakartaee.petclinic.it.api.*;
     VisitEndpointIT.class, OwnerEndpointIT.class
 })
 public class EndpointTestSuite {
+
+    @BeforeAll
+    public void runBeforeSuite(){
+        log.info("START");
+    }
+
+    @AfterAll
+    public void runAfterSuite(){
+        log.info("DONE");
+    }
 }
