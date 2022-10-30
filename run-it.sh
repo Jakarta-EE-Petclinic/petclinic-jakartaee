f2ea873afddf29902392a3fc18f67d212a39e395
thomaswoehlke <thomas.woehlke@gmail.com>
2022-10-30 13:24:39 +0100

    Jakarta EE 10

--- run-it.sh
+++ run-it.sh
@@ -3,7 +3,8 @@
 export POSTGRESQL_HOST=localhost
 export POSTGRESQL_PORT=5432
 export POSTGRESQL_DATABASE=petclinic_jakartaee_wildfly
-export POSTGRESQL_USER=petclinic_jakartaee                                                                                                                                                                                                                                                                                                                                                                                                                                                                aee
+export POSTGRESQL_USER=petclinic_jakartaee
+
 export POSTGRESQL_PASSWORD=petclinic_jakartaeepwd
 
 ./mvnw -Parq-wildfly-managed
