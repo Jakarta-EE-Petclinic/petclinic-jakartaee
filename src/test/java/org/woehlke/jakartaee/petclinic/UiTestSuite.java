ca2c2199137cb30682de8ed0d86a932155dc4dd0
thomaswoehlke <thomas.woehlke@gmail.com>
2022-11-05 00:58:08 +0100

    Jakarta EE 10: Payara 6.2022.1 Community Edition

--- src/test/java/org/woehlke/jakartaee/petclinic/UiTestSuite.java
+++ src/test/java/org/woehlke/jakartaee/petclinic/UiTestSuite.java
@@ -5,17 +5,19 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
+import org.woehlke.jakartaee.petclinic.it.api.RestEndpointIT;
 import org.woehlke.jakartaee.petclinic.it.ui.*;
 
 @Log
 @Suite
 @SelectClasses({
+    RestEndpointIT.class,
     SpecialtyUiTest.class,
     VetUiTest.class,
     PetTypeUiTest.class,
-    OwnerUiTest.class,
-    InformationUiTest.class,
-    TellTheStoryUiTest.class
+    OwnerUiTest.class//,
+    //InformationUiTest.class,
+    //TellTheStoryUiTest.class
 })
 public class UiTestSuite {
 
